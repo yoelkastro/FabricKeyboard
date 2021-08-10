796e4be97179ce9b97dc40790c4503908d6c387a
yoelkastro <yoelkastro@gmail.com>
2021-08-10 22:28:21 +0300

    Add comments

--- src/MixedOutputTest/MixedOutputTest.ino
+++ src/MixedOutputTest/MixedOutputTest.ino
@@ -1,3 +1,11 @@
+/*
+ * MixedOutputTest
+ * 
+ * Script for testing the two output methods the keyboard should have: USB and BLE.
+ * The BLE_DEVICE constant determines which of these is used: true for BLE and false for USB.
+ *
+ */
+
 #define BLE_DEVICE true
 
 #include <BLEMIDI_Transport.h>
@@ -11,6 +19,14 @@ BLEMIDI_CREATE_DEFAULT_INSTANCE();
 
 bool isConnected;
 
+/*
+ * Sends a MIDI note on event over the selected output method.
+ * 
+ * @param channel:  The MIDI channel the note should be played on.
+ * @param note:		The note that should be played.
+ * @param velocity:	The velocity of the note.
+ * 
+ */
 void noteOn(byte channel, byte note, byte velocity){
 	if(BLE_DEVICE){
 		MIDI.sendNoteOn(note, velocity, channel);
@@ -20,11 +36,14 @@ void noteOn(byte channel, byte note, byte velocity){
 	}
 }
 
-void onConnected(){isConnected = true;}
-void onDisconnected(){isConnected = false;}
-void OnNoteOn(byte channel, byte note, byte velocity) {}
-void OnNoteOff(byte channel, byte note, byte velocity) {}
-
+/*
+ * Sends a MIDI note off event over the selected output method.
+ * 
+ * @param channel:  The MIDI channel the note should be turned off on.
+ * @param note:		The note that should be turned off.
+ * @param velocity:	The velocity of the note.
+ * 
+ */
 void noteOff(byte channel, byte note, byte velocity){
 	if(BLE_DEVICE){
 		MIDI.sendNoteOff(note, velocity, channel);
@@ -35,16 +54,23 @@ void noteOff(byte channel, byte note, byte velocity){
   
 }
 
+/*
+void onConnected(){isConnected = true;}
+void onDisconnected(){isConnected = false;}
+void OnNoteOn(byte channel, byte note, byte velocity) {}
+void OnNoteOff(byte channel, byte note, byte velocity) {}
+*/
+
 void setup(){
 
 	if(BLE_DEVICE){
 		MIDI.begin();
 
-		BLEMIDI.setHandleConnected(onConnected);
+		/*BLEMIDI.setHandleConnected(onConnected);
   		BLEMIDI.setHandleDisconnected(onDisconnected);
 
   		MIDI.setHandleNoteOn(OnNoteOn);
-  		MIDI.setHandleNoteOff(OnNoteOff);
+  		MIDI.setHandleNoteOff(OnNoteOff);*/
 	}
 	else{
 		Control_Surface.begin();
